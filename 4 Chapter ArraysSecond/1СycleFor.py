9c034601c0db77bd905fc2733f3f001168c06e88
Freeman699 <mr.frimen1324@gmail.com>
2021-10-28 18:10:04 +0300

    Chapter4 first commit

---
+++ 4 Chapter ArraysSecond/1СycleFor.py
@@ -0,0 +1,12 @@
+pizzas = ["Mozzarella", "Hawaiian", "Diablo"]
+
+for pizza_name in pizzas:
+    print(f"I like {pizza_name} pizza")
+print("\nI realy like pizza!\n")
+
+
+animals = ["squirrel", "bird", "raccoon"]
+
+for animal in animals:
+    print(f"A {animal} would make a great pet")
+print("\nAny of this animals would make a great pet\n")
---
+++ 4 Chapter ArraysSecond/2RangeAndListGenerator.py
@@ -0,0 +1,29 @@
+numbers_20 = []
+for value in range(1,21):
+    numbers_20.append(value)
+print(numbers_20)
+
+one_million_array = []
+for value in range (1, 1000000 + 1):
+    one_million_array.append(value)
+print(min(one_million_array))
+print(max(one_million_array))
+print(sum(one_million_array))
+
+even_numbers = []
+for value in range (1, 21, 2):
+    even_numbers.append(value)
+print(even_numbers)
+
+mod_three_numbers = []
+for value in range (1,11):
+    mod_three_numbers.append(value*3)
+print(mod_three_numbers)
+
+сubic_numbers = []
+for value in range (1,11):
+    сubic_numbers.append(value**3)
+print(сubic_numbers)
+
+generator_cubic_numbers = [value**3 for value in range(1,11)]
+print(generator_cubic_numbers)
